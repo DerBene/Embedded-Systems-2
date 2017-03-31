8d29e9e3a4cb00834a2181c8a3899b12e9d89a9e
DerBene <pvpbene@googlemail.com>
2017-03-31 10:17:39 +0200

    Added main Structure

--- src/Printf.cpp
+++ src/Printf.cpp
@@ -1,9 +1,58 @@
 #include "../include/Printf.h"
-
+#define unsignedInt 'u'
+#define signedInt 'd'
+#define singleChar 'c'
+#define charChain 's'
+#define hex 'x'
+#define bin 'b'
+#define percent '&'
 
 char* Printf(char* dst, const void* end, const char* fmt, ...){
-    (void) dst;
-    (void) end;
-    (void) fmt;
-    return nullptr;
+    char* inputString = dst;
+
+    while(*inputString != '\0'){
+        if(*inputString == '%'){
+            inputString++;
+            switch(*inputString){
+                case unsignedInt :
+                {
+                    //ToDo: Logic
+                }break;
+
+                case signedInt :
+                {
+                    //ToDo: Logic
+                }break;
+
+                case singleChar :
+                {
+                    //ToDo: Logic
+                }break;
+
+                case charChain :
+                {
+                    //ToDo: Logic
+                }break;
+
+                case hex :
+                {
+                    //ToDo: Logic
+                }break;
+
+                case bin :
+                {
+                    //ToDo: Logic
+                }break;
+
+                case percent :
+                {
+                    //ToDo: Logic
+                }break;
+            }
+        }else{
+
+        }
+        inputString++;
+    }
+
 }
